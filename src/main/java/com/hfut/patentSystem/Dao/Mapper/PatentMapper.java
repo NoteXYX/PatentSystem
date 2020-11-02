e7c03be63aa6c215dcceed7c5875089c05465329
NoteXYX <707997228@qq.com>
2020-11-03 00:03:57 +0800

    ok

--- src/main/java/com/hfut/patentSystem/Dao/Mapper/PatentMapper.java
+++ src/main/java/com/hfut/patentSystem/Dao/Mapper/PatentMapper.java
@@ -3,7 +3,6 @@ package com.hfut.patentSystem.Dao.Mapper;
 import com.hfut.patentSystem.Dao.Entity.Patent;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface PatentMapper {
