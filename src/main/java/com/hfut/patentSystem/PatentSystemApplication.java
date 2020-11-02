8cfd09855b91dacc260de0da9c918af0b0da5a3a
xyx <707997228@qq.com>
2020-11-02 20:34:01 +0800

    Initial commit

---
+++ src/main/java/com/hfut/patentSystem/PatentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hfut.patentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hfut/patentSystem/PatentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfut.patentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
